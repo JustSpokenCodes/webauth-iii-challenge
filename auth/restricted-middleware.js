0ed8f887c859cd561b5fe775a0a09ae196b4b7d9
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-29 19:58:08 -0700

    with the teacher, i made it work

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -8,8 +8,8 @@ module.exports = (req, res, next) => {
 
     if (tokenHeader) {
         const tokenStrings = tokenHeader.split(" ");
-        if (tokenStrings [0].toUpperCase() === 'BEARER' && tokenStrings[1]) {
-            jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        
+            jwt.verify(tokenHeader, secrets.jwtSecret, (err, decodedToken) => {
                 if (err) {
                     res.status(401).json({message: 'verifying token is coming up error wise', error: err});
                 } else {
@@ -17,9 +17,6 @@ module.exports = (req, res, next) => {
                     next();
                 }
             });
-        } else {
-            res.status(401).json({message: 'invalid scheme, or no token after scheme name'})
-        }
     } else {
         res.status(401).json({message: 'missing Auth header'});
     }
